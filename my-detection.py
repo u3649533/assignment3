8b320f08de04c5825ddc3e187fceae9a25081d7a
u3649533 <u3649533@connect.hku.hk>
2025-09-27 10:26:51 +0800

    Add files via upload

--- my-detection.py
+++ my-detection.py
@@ -4,7 +4,8 @@ from jetson_utils import videoSource, videoOutput
 #import jetson_utils
 
 net = detectNet("ssd-mobilenet-v2", threshold=0.5)
-camera = videoSource("/dev/video0") # '/dev/video0' for V4L2
+camera = videoSource("/home/nvidia/Pictures/2.jpeg") # '/dev/video0' for V4L2
+#image_path = "/home/nvidia/Pictures/1.jpeg"  
 display = videoOutput("display://0") # 'my_video.mp4' for f
 
 while display.IsStreaming(): # main loop will go here
@@ -15,3 +16,4 @@ while display.IsStreaming(): # main loop will go here
       print(detections)
       display.Render(img)
       display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+      
